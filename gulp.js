7befbf5931dc2e4d957cc90615393f9c448c642e
Stjarnan <stiernstrom_3@hotmail.com>
2016-09-28 23:55:42 +0200

    Changed name of folder

---
+++ gulp.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var clean = require("del");
+var concat = require("gulp-concat");
+
+gulp.task("cleanDist", function(){
+    return gulp.src();
+});
